ea8fe04ef3e782cec7ae15a82c893277a89f8b28
Sonam Kadam <125948681+sonamkadam25@users.noreply.github.com>
2023-09-27 21:38:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("task");
+    const addTaskButton = document.getElementById("addTask");
+    const taskList = document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click", function () {
+        const taskText = taskInput.value.trim();
+        if (taskText !== "") {
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `
+                ${taskText}
+                <button class="delete-button">Delete</button>
+            `;
+            taskList.appendChild(listItem);
+            taskInput.value = "";
+
+            const deleteButton = listItem.querySelector(".delete-button");
+            deleteButton.addEventListener("click", function () {
+                taskList.removeChild(listItem);
+            });
+        }
+    });
+});
